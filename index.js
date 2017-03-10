b5c36de2b8f6f4bbb392ba4f9a376a8eb1aefb59
ken1987 <ken198701@gmail.com>
2017-03-10 10:44:55 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-'use strict';
+'use strict'
 
-var minifier = require('html-minifier');
-var minify = minifier.minify;
+var minifier = require('html-minifier')
+var minify = minifier.minify
 
 module.exports = function(content, file, conf) {
     if (file.isHtmlLike) {
-        console.error(typeof minifier);
-        console.error(typeof minify);
-        return minify(content, conf);
+        return minify(content, conf)
     }
-    return content;
-};
+    return content
+}
